48e639bdfe571eeafc5f858b9f9cde8d5b369155
Seiya Ito <42412743+Soiya-kun@users.noreply.github.com>
2025-07-03 17:28:26 +0900

    Add FrontendStack with CloudFront deployment

--- infra/bin/app.ts
+++ infra/bin/app.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { BackendStack } from '../lib/BackendStack';
+import { FrontendStack } from '../lib/FrontendStack';
 
 const app = new cdk.App();
 new BackendStack(app, 'BackendStack');
+new FrontendStack(app, 'FrontendStack');
